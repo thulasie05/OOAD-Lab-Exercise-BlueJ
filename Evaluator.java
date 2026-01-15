0b2784808aded72235e8103b7dc4398d114e4831
Angela Wong <angelawong877@gmail.com>
2026-01-15 11:10:23 +0800

    BlueJ Files

---
+++ Evaluator.java
@@ -0,0 +1,34 @@
+
+/**
+ * Write a description of class Evaluator here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Evaluator extends User
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class Evaluator
+     */
+    public Evaluator()
+    {
+        // initialise instance variables
+        x = 0;
+        Evaluation obj = new Evaluation();
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
