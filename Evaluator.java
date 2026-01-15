d96b769de950f34c8ed057eb0589e9a18fb22c4c
Angela Wong <angelawong877@gmail.com>
2026-01-15 16:22:52 +0800

    Update Evaluator.java

--- Evaluator.java
+++ Evaluator.java
@@ -1,34 +1,15 @@
+public class Evaluator extends User {
 
-/**
- * Write a description of class Evaluator here.
- *
- * @author (your name)
- * @version (a version number or a date)
- */
-public class Evaluator extends User
-{
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class Evaluator
-     */
-    public Evaluator()
-    {
-        // initialise instance variables
-        x = 0;
-        Evaluation obj = new Evaluation();
+    public Evaluator(String userId, String name, String email) {
+        super(userId, name, email);
     }
 
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+    public void reviewPresentation() {
+    }
+
+    public void evaluateStudents() {
+    }
+
+    public void comment() {
     }
 }
